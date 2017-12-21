0fb4100018485171c862bf748197e24eaefc907d
Jan Ulferts <julferts@mesosphere.com>
2017-12-21 11:37:55 +0100

    added certificate handling

--- dcos/client/client.go
+++ dcos/client/client.go
@@ -1,8 +1,19 @@
 package client
 
 import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
 	"errors"
+	"fmt"
+	"io/ioutil"
+	"net"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/mesosphere/dcos-go/dcos/config"
 )
@@ -45,12 +56,123 @@ func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	return res, nil
 }
 
-// NewClient creates an *http.Client from an DCOS configuration *config.Config
-func NewClient(conf *config.Config) *http.Client {
-	c := http.Client{
+type APIClient struct {
+	HTTPClient *http.Client
+}
+
+// NewAPIClient creates a new APIClient and adds a HTTPclient from Config
+func NewAPIClient(conf *config.Config) *APIClient {
+	c := APIClient{}
+	c.HTTPClient = NewHttpClient(conf)
+	return &c
+}
+
+// NewAPIClient creates a new APIClient and adds a HTTPclient from Config
+func NewAPIClientWithHTTPClient(hc *http.Client) *APIClient {
+	c := APIClient{}
+	c.HTTPClient = hc
+	return &c
+}
+
+func (a *APIClient) Post(url, contentType, accept string, input, output interface{}) (err error) {
+	in, err := json.Marshal(input)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(in))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Accept", accept)
+	req.Header.Add("Content-Type", contentType)
+
+	resp, err := a.HTTPClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode == http.StatusUnauthorized {
+		return fmt.Errorf("Unauthorized. You're not allowed to request this enpoint")
+	}
+
+	if resp.StatusCode >= 400 {
+		b, _ := ioutil.ReadAll(resp.Body)
+		return fmt.Errorf("Request error got: %d - body: %s", resp.StatusCode, b)
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(b, output)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (a *APIClient) Get(url, output interface{}) (err error) {
+	return nil
+}
+
+// NewHttpClient creates an *http.Client from an DCOS configuration *config.Config
+func NewHttpClient(conf *config.Config) *http.Client {
+	// Taken from http.DefaultTransport
+	var transport http.RoundTripper = &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		TLSClientConfig:       &tls.Config{},
+	}
+
+	if conf.Core != nil && conf.Core.SslVerify != "" {
+
+		sslVerify, err := strconv.ParseBool(conf.Core.SslVerify)
+		if err != nil {
+			// we expect a file
+			if _, err := os.Stat(conf.Core.SslVerify); os.IsNotExist(err) {
+				fmt.Fprintf(os.Stderr, "dcos-go fatal: could not find specied certificate at %s", conf.Core.SslVerify)
+				return nil
+			}
+			certfile, err := ioutil.ReadFile(conf.Core.SslVerify)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dcos-go fatal: read certificate from %s", conf.Core.SslVerify)
+				return nil
+			}
+			p, _ := pem.Decode(certfile)
+			if p == nil {
+				fmt.Fprintf(os.Stderr, "dcos-go fatal: not a pem file %s", conf.Core.SslVerify)
+				return nil
+			}
+			cert, err := x509.ParseCertificate(p.Bytes)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dcos-go fatal: parse certificate from %s - %s", conf.Core.SslVerify, err)
+				return nil
+			}
+
+			transport.(*http.Transport).TLSClientConfig.RootCAs = x509.NewCertPool()
+			transport.(*http.Transport).TLSClientConfig.RootCAs.AddCert(cert)
+		} else {
+			transport.(*http.Transport).TLSClientConfig.RootCAs = nil
+			transport.(*http.Transport).TLSClientConfig.InsecureSkipVerify = !sslVerify
+		}
+	}
+
+	return &http.Client{
 		Transport: &Transport{
 			Config: conf,
+			Base:   transport,
 		},
 	}
-	return &c
 }
--- dcos/client/client_test.go
+++ dcos/client/client_test.go
@@ -1,59 +1,245 @@
 package client
 
 import (
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/mesosphere/dcos-go/dcos/config"
 	"github.com/stretchr/testify/assert"
-	"gopkg.in/h2non/gock.v1"
 )
 
 // Lets do an samle request and see if we find the token header
 func TestNewClient(t *testing.T) {
-	testurl := "http://foobar.com/baz"
 	testtoken := "Testtoken"
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Authorization") == "token="+testtoken {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("TestNewClient - Success!"))
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("TestNewClient - Forbidden!"))
+		}
+	}))
+	defer s.Close()
+	testurl := s.URL
 	conf := config.Config{
 		Core: &config.Core{
 			DcosUrl:      testurl,
 			DcosAcsToken: testtoken,
 		},
 	}
-	gock.New(testurl).
-		MatchHeader("Authorization", "^token="+testtoken+"$").
-		Reply(200).
-		BodyString("success")
-
-	// return 401 if auth is not present
-	gock.New(testurl).
-		Reply(401).
-		BodyString("denied")
-
-	c := NewClient(&conf)
+	c := NewHttpClient(&conf)
 	resp, err := c.Get(testurl)
 	assert.NoError(t, err)
 	assert.Equal(t, 200, resp.StatusCode)
-	//just test our mock
-	c = http.DefaultClient
-	resp, err = c.Get(testurl)
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	assert.Contains(t, string(respBody), "Success")
+
+	respDflt, err := http.DefaultClient.Get(testurl)
 	assert.NoError(t, err)
-	assert.Equal(t, 401, resp.StatusCode)
+	assert.Equal(t, 401, respDflt.StatusCode)
+	respDfltbody, _ := ioutil.ReadAll(respDflt.Body)
+	assert.Contains(t, string(respDfltbody), "Forbidden")
 }
 
 func TestNewClientInvalidConf(t *testing.T) {
-	testurl := "http://foobar.com/baz"
-
-	gock.New(testurl).
-		Reply(201).
-		BodyString("success")
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}))
+	testurl := s.URL
 
 	conf := config.Config{}
 
 	assert.False(t, conf.Valid())
 
-	c := NewClient(&conf)
+	c := NewHttpClient(&conf)
 	resp, err := c.Get(testurl)
 	assert.Error(t, err)
 	assert.Nil(t, resp)
+}
+func TestNewClientNotExistingCert(t *testing.T) {
+	file, err := ioutil.TempFile(os.TempDir(), "dcos-go-not-existing")
+	assert.NoError(t, err)
+	os.Remove(file.Name())
 
+	conf := config.Config{
+		Core: &config.Core{
+			SslVerify:    file.Name(),
+			DcosUrl:      "http://foo.bar.baz",
+			DcosAcsToken: "foobar",
+		},
+	}
+
+	assert.True(t, conf.Valid())
+
+	c := NewHttpClient(&conf)
+	assert.Nil(t, c)
+}
+
+func TestNewClientInvalidPermissions(t *testing.T) {
+	file, err := ioutil.TempFile(os.TempDir(), "dcos-go-invalid-permissions")
+	assert.NoError(t, err)
+	file.Chmod(0222)
+
+	conf := config.Config{
+		Core: &config.Core{
+			SslVerify:    file.Name(),
+			DcosUrl:      "http://foo.bar.baz",
+			DcosAcsToken: "foobar",
+		},
+	}
+
+	assert.True(t, conf.Valid())
+
+	c := NewHttpClient(&conf)
+	assert.Nil(t, c)
+}
+
+func TestNewClientInvalidPEM(t *testing.T) {
+	file, err := ioutil.TempFile(os.TempDir(), "dcos-go-invalid-pem")
+	assert.NoError(t, err)
+
+	conf := config.Config{
+		Core: &config.Core{
+			SslVerify:    file.Name(),
+			DcosUrl:      "http://foo.bar.baz",
+			DcosAcsToken: "foobar",
+		},
+	}
+
+	assert.True(t, conf.Valid())
+
+	c := NewHttpClient(&conf)
+	assert.Nil(t, c)
+}
+
+func TestNewClientInvalidCert(t *testing.T) {
+	file, err := ioutil.TempFile(os.TempDir(), "dcos-go-invalid-cert")
+	assert.NoError(t, err)
+	b := pem.Block{
+		Type:  "foobar",
+		Bytes: []byte("Invalid Cert"),
+	}
+	out := pem.EncodeToMemory(&b)
+
+	file.Write(out)
+
+	conf := config.Config{
+		Core: &config.Core{
+			SslVerify:    file.Name(),
+			DcosUrl:      "http://foo.bar.baz",
+			DcosAcsToken: "foobar",
+		},
+	}
+
+	assert.True(t, conf.Valid())
+
+	c := NewHttpClient(&conf)
+	assert.Nil(t, c)
+}
+
+func TestNewClientWithandWithoutVerify(t *testing.T) {
+	cVerify := NewHttpClient(&config.Config{
+		Core: &config.Core{
+			SslVerify: "true",
+		},
+	})
+	cNoVerify := NewHttpClient(&config.Config{
+		Core: &config.Core{
+			SslVerify: "false",
+		},
+	})
+
+	assert.False(t, cVerify.Transport.(*Transport).Base.(*http.Transport).TLSClientConfig.InsecureSkipVerify)
+	assert.True(t, cNoVerify.Transport.(*Transport).Base.(*http.Transport).TLSClientConfig.InsecureSkipVerify)
+}
+
+func TestNewClientWithSelfSignedSSL(t *testing.T) {
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "TestNewClientWithSelfSignedSSL")
+	}))
+	defer ts.Close()
+
+	cert := ts.Certificate()
+
+	file, err := ioutil.TempFile(os.TempDir(), "dcos-go-ssl-ca")
+	assert.NoError(t, err)
+	b := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: cert.Raw})
+
+	_, err = file.Write(b)
+	assert.NoError(t, err)
+
+	testurl := "http://foobar.com/baz"
+	testtoken := "Testtoken"
+	conf := config.Config{
+		Core: &config.Core{
+			DcosUrl:      testurl,
+			DcosAcsToken: testtoken,
+			SslVerify:    file.Name(),
+		},
+	}
+	c := NewHttpClient(&conf)
+
+	resp, err := c.Get(ts.URL)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 200, resp.StatusCode)
+
+	// lets check we really get an error
+	conf1 := config.Config{
+		Core: &config.Core{
+			DcosUrl:      testurl,
+			DcosAcsToken: testtoken,
+			SslVerify:    "true",
+		},
+	}
+
+	c1 := NewHttpClient(&conf1)
+	_, err1 := c1.Get(ts.URL)
+	assert.Error(t, err1)
+	fmt.Printf(ts.URL)
+	ts.Close()
+}
+
+func TestNewClientWithoutSSLVerify(t *testing.T) {
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "TestNewClientWithoutSSLVerify")
+	}))
+	defer ts.Close()
+
+	testurl := "http://foobar.com/baz"
+	testtoken := "Testtoken"
+
+	conf := config.Config{
+		Core: &config.Core{
+			DcosUrl:      testurl,
+			DcosAcsToken: testtoken,
+			SslVerify:    "false",
+		},
+	}
+
+	conf1 := config.Config{
+		Core: &config.Core{
+			DcosUrl:      testurl,
+			DcosAcsToken: testtoken,
+			SslVerify:    "true",
+		},
+	}
+
+	c := NewHttpClient(&conf)
+	c1 := NewHttpClient(&conf1)
+
+	resp, err := c.Get(ts.URL)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 200, resp.StatusCode)
+
+	_, err = c1.Get(ts.URL)
+	assert.Error(t, err)
 }
