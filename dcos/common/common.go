3a634509dc9c4e2d81a7f9687d524ea7eabee740
Jan Ulferts <julferts@mesosphere.com>
2019-01-03 15:50:05 +0100

    improved service methods. Added requestError

--- dcos/common/common.go
+++ dcos/common/common.go
@@ -18,6 +18,24 @@ type Service struct {
 	Path   string
 }
 
+type RequestError struct {
+	err    string
+	status int
+	body   []byte
+}
+
+func (e *RequestError) Error() string {
+	return fmt.Sprintf("Code: %d - %s", e.status, e.err)
+}
+
+func (e *RequestError) Body() []byte {
+	return e.body
+}
+
+func (e *RequestError) StatusCode() int {
+	return e.status
+}
+
 type Job struct {
 }
 
@@ -28,6 +46,56 @@ func (s *Service) GetServiceURL(path string) *url.URL {
 }
 
 func (s *Service) GetJSON(path string, object interface{}) error {
+	resp, err := s.Client.Get(s.GetServiceURL(path).String())
+	if err != nil {
+		return err
+	}
+
+	recBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != 200 {
+		return &RequestError{"HTTP request not successful", resp.StatusCode, recBody}
+	}
+
+	err = json.Unmarshal(recBody, object)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Service) Delete(path string) error {
+	req := &http.Request{}
+	req.URL = s.GetServiceURL(path)
+	req.Method = "DELETE"
+	resp, err := s.Client.Do(req)
+
+	if err != nil {
+		return err
+	}
+
+	recBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode == 401 || resp.StatusCode == 403 {
+		return &RequestError{"Not authorized", resp.StatusCode, recBody}
+	}
+
+	if resp.StatusCode == 404 {
+		return &RequestError{"Job not found", resp.StatusCode, recBody}
+	}
+
+	if resp.StatusCode >= 400 {
+		return &RequestError{"Unknown error", resp.StatusCode, recBody}
+	}
+
 	return nil
 }
 
@@ -42,13 +110,44 @@ func (s *Service) PostJSON(path string, sentObject, receiveObject interface{}) e
 		return err
 	}
 
+	recBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
 	if resp.StatusCode >= 400 {
-		return fmt.Errorf("Post respond with status: %d", resp.StatusCode)
+		return &RequestError{"Post respond with 4xx", resp.StatusCode, recBody}
+	}
+
+	err = json.Unmarshal(recBody, receiveObject)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Service) PutJSON(path string, sentObject, receiveObject interface{}) error {
+	body, err := json.Marshal(sentObject)
+	if err != nil {
+		return err
+	}
+	req, err := http.NewRequest("PUT", s.GetServiceURL(path).String(), bytes.NewBuffer(body))
+
+	req.Header.Set("content-type", "application/json")
+
+	resp, err := s.Client.Do(req)
+	if err != nil {
+		return err
 	}
 
 	recBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return err
+	}
+
+	if resp.StatusCode >= 400 {
+		return &RequestError{"Post respond with 4xx", resp.StatusCode, recBody}
 	}
 
 	err = json.Unmarshal(recBody, receiveObject)
