d47648a7bc8c2d48883ca30e638bb0ebdd9832bd
Jan Ulferts <julferts@mesosphere.com>
2019-02-20 07:01:28 +0100

    reverting config changes

--- dcos/config.go
+++ dcos/config.go
@@ -1,20 +1,3 @@
 package dcos
 
-import "crypto/tls"
-
-type Config struct {
-	// TODO
-	Authentication Authentication
-}
-
-func (c *Config) TLS() *tls.Config {
-	// if SslVerify is bool and false
-	// roundtripper.(*http.Transport).TLSClientConfig.InsecureSkipVerify = false
-	return &tls.Config{
-		InsecureSkipVerify: false,
-	}
-
-	// otherwise add cert
-	// transport.(*http.Transport).TLSClientConfig.RootCAs = x509.NewCertPool()
-	// transport.(*http.Transport).TLSClientConfig.RootCAs.AddCert(cert)
-}
+type Config struct{}
