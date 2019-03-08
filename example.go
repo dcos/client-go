90f11fb749ad73104972ce4a3ec4b380b85c5c6c
Thorsten Ball <mrnugget@gmail.com>
2019-03-08 14:07:13 +0100

    Re-enable Secrets example

--- example.go
+++ example.go
@@ -44,45 +44,38 @@ func login(clusterUrl, username, password string) (string, error) {
 	return authToken.Token, nil
 }
 
-/*
-func createSecret(client *dcos.Client, secretName, secretValue string) error {
-	paramsSecret := secrets.
-		NewPutSecretStorePathToSecretParams().
-		WithStore("default").
-		WithPathToSecret(secretName).
-		WithBody(&secretsmodels.Secret{Value: secretValue})
+func createSecret(client *dcos.APIClient, secretName, secretValue string) error {
+	secret := dcos.Secret{Value: secretValue}
 
-	result, err := client.Secrets.Secrets.PutSecretStorePathToSecret(paramsSecret)
+	_, err := client.SecretsApi.CreateSecret(context.TODO(), "default", secretName, secret)
 	if err != nil {
-		switch err.(type) {
-		case *secrets.PutSecretStorePathToSecretConflict:
-			log.Printf("Secret %q already created", secretName)
+		switch err := err.(type) {
+		case dcos.GenericOpenAPIError:
+			if err.Error() == "409 Conflict" {
+				log.Printf("Secret %s already created", secretName)
+				return nil
+			}
+			return err
 		default:
 			return err
 		}
 	}
 
-	log.Printf("Secret created: %+v\n", result)
+	log.Printf("Secret created: %+v\n", secretName)
 
 	return nil
 }
 
-func getSecret(client *dcos.Client, secretName string) error {
-	paramsSecret := secrets.
-		NewGetSecretStorePathToSecretParams().
-		WithStore("default").
-		WithPathToSecret(secretName)
-
-	result, err := client.Secrets.Secrets.GetSecretStorePathToSecret(paramsSecret)
+func getSecret(client *dcos.APIClient, secretName string) error {
+	secret, _, err := client.SecretsApi.GetSecret(context.TODO(), "default", secretName, nil)
 	if err != nil {
 		return err
 	}
 
-	log.Printf("Secret fetched: %+v\n", result)
+	log.Printf("Secret fetched: %+v\n", secret)
 
 	return nil
 }
-*/
 
 func installPackage(client *dcos.APIClient, packageName string) error {
 	result, _, err := client.CosmosApi.PackageInstall(context.TODO(), dcos.InstallRequest{
@@ -126,47 +119,26 @@ func main() {
 		log.Printf("Logged in and received token: %s", token)
 	}
 
-	/*
-		config.SetACSToken(token)
+	err = createSecret(client, "biggestsecret3", "i am a dog")
+	if err != nil {
+		log.Fatalf("Creating secrets failed: %s\n", err)
+	}
 
-		httpClient, err = dcos.NewHTTPClient(config)
-		if err != nil {
-			log.Fatalf("Creating new HTTP client failed: %s\n", err)
-		}
+	err = getSecret(client, "biggestsecret3")
+	if err != nil {
+		log.Fatalf("Creating secrets failed: %s\n", err)
+	}
 
-		authClient, err := dcos.NewClientWithOptions(httpClient, config)
-		if err != nil {
-			log.Fatalf("Creating new client failed: %s\n", err)
-		}
-
-		log.Printf("Login successful")
-
-		err = listUsers(authClient)
-		if err != nil {
-			log.Fatalf("Listing users failed: %s\n", err)
-		}
-
-		err = createSecret(authClient, "biggestsecret3", "i am a dog")
-		if err != nil {
-			log.Fatalf("Creating secrets failed: %s\n", err)
-		}
-
-		err = getSecret(authClient, "biggestsecret3")
-		if err != nil {
-			log.Fatalf("Creating secrets failed: %s\n", err)
-		}
-
-		err = installPackage(authClient, "marathon-lb", nil)
-		if err != nil {
-			log.Fatalf("Installing package failed: %s\n", err)
-		}
-
-		jenkinsOptions := &cosmos.PackageOptions{
-			Security: cosmos.PackageSecurityOptions{
-				StrictMode: true,
-			},
-		}
-	*/
+	// err = installPackage(authClient, "marathon-lb", nil)
+	// if err != nil {
+	// 	log.Fatalf("Installing package failed: %s\n", err)
+	// }
+	//
+	// jenkinsOptions := &cosmos.PackageOptions{
+	// 	Security: cosmos.PackageSecurityOptions{
+	// 		StrictMode: true,
+	// 	},
+	// }
 
 	err = installPackage(client, "jenkins")
 	if err != nil {
